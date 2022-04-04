43e85d043bcf4cb2fac0fdb4c5c802fe860e8181
Hugo Dias Giló <80465879+hugodiasg@users.noreply.github.com>
2022-04-04 10:35:06 -0300

    Update 6-klayout.sh

--- 6-klayout.sh
+++ 6-klayout.sh
@@ -1,7 +1,10 @@
 echo "Install Klayout sky130 tech"
-cd /home/$(whoami)/.klayout
+cd
+cd .klayout
 mkdir tech
 cd tech
 git clone https://github.com/mabrains/sky130_klayout_pdk.git sky130
 pip install pandas
+cd
+cd sky130_workspace
 echo "END"
