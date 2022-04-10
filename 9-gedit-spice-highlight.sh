05d1c37ce870349f84253185f1f4157a08625951
Hugo Dias Giló <80465879+hugodiasg@users.noreply.github.com>
2022-04-10 12:42:00 -0300

    Create 9-gedit-spice-highlight.sh

---
+++ 9-gedit-spice-highlight.sh
@@ -0,0 +1,7 @@
+echo "Install Gedit Spice Highlight"
+git clone https://github.com/thomasysliu/Gedit-SPICE-syntax-highlighting
+cd Gedit-SPICE-syntax-highlighting/
+sudo cp spice.lang /usr/share/gtksourceview-4/language-specs/
+cd ..
+
+
