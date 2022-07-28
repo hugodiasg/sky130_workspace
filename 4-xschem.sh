a651afbd64b16b0d3aa7fe705821e6f8a9a84175
Hugo Dias Giló <80465879+hugodiasg@users.noreply.github.com>
2022-07-28 11:14:45 -0300

    Update 4-xschem.sh

--- 4-xschem.sh
+++ 4-xschem.sh
@@ -16,11 +16,13 @@ cd xschem_library/
 git clone https://github.com/StefanSchippers/xschem_sky130
 cd /home/$(whoami)/sky130_workspace/
 ln -s /home/$(whoami)/.xschem/xschem_library/xschem_sky130
+ln -s /home/$(whoami)/sky130_workspace/skywater-pdk/libraries/sky130_fd_pr /usr/local/share/pdk/sky130A/libs.ref
+
 
 # config pdk to xschem and ngspice
 echo "Config pdk to xschem and ngspice"
-cd /home/$(whoami)/sky130_workspace/skywater-pdk/libraries
-cp -a sky130_fd_pr sky130_fd_pr_ngspice
-cd sky130_fd_pr_ngspice/latest/
-patch -p2 </home/$(whoami)/.xschem/xschem_library/xschem_sky130/sky130_fd_pr.patch
-cd ../../../..
+*cd /home/$(whoami)/sky130_workspace/skywater-pdk/libraries
+*cp -a sky130_fd_pr sky130_fd_pr_ngspice
+*cd sky130_fd_pr_ngspice/latest/
+*patch -p2 </home/$(whoami)/.xschem/xschem_library/xschem_sky130/sky130_fd_pr.patch
+*cd ../../../..
