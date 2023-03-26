1a9e1f564b3879897d5842687e2d420d8870cd9c
Hugo Dias Giló <80465879+hugodiasg@users.noreply.github.com>
2023-03-26 01:07:25 -0300

    Update 12-openlane.sh

--- 12-openlane.sh
+++ 12-openlane.sh
@@ -1,5 +1,6 @@
 # Openlane
 sudo apt install python3.10-venv curl -y
+sudo apt-get install tcllib -y
 cd openlane
 
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
@@ -13,5 +14,9 @@ sudo usermod -aG docker $USER
 sudo chmod 666 /var/run/docker.sock
 
 make
+export PDK_ROOT=/home/hugodg/sky130_workspace/open_pdks/sky130/
+sudo rm -rf /home/hugodg/sky130_workspace/open_pdks/sky130/sky130A
+sudo rm -rf /home/hugodg/sky130_workspace/open_pdks/sky130/sky130B
+make pdk
 make test
-cd ..
+cd ..                
