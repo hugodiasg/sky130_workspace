306283b68d10b9caf1ac73f1229db136676f017d
Hugo Dias Giló <80465879+hugodiasg@users.noreply.github.com>
2023-03-26 15:33:04 -0300

    Update 3-magic.sh

--- 3-magic.sh
+++ 3-magic.sh
@@ -17,4 +17,4 @@ sudo make
 sudo make install
 cd ../..
 sudo ln -s /home/hugodg/sky130_workspace/open_pdks/sky130/sky130A/libs.tech/magic/* /usr/local/lib/magic/sys/
-
+export PDK_ROOT=/home/$(whoami)/sky130_workspace/open_pdks/sky130
