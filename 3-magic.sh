d4c67983052879f492de8d7f44afa46833e5527d
Hugo Dias Giló <80465879+hugodiasg@users.noreply.github.com>
2023-07-24 14:03:31 -0600

    Update 3-magic.sh

--- 3-magic.sh
+++ 3-magic.sh
@@ -16,5 +16,5 @@ sudo ./configure --enable-sky130-pdk=/home/$(whoami)/sky130_workspace/skywater-p
 sudo make
 sudo make install
 cd ../..
-sudo ln -s /home/hugodg/sky130_workspace/open_pdks/sky130/sky130A/libs.tech/magic/* /usr/local/lib/magic/sys/
+sudo ln -s /home/$(whoami)/sky130_workspace/open_pdks/sky130/sky130A/libs.tech/magic/* /usr/local/lib/magic/sys/
 export PDK_ROOT=/home/$(whoami)/sky130_workspace/open_pdks/sky130
