e91b4bb49c2b2e48b2f9ae5364b26dc97ecc0197
Hugo Dias Giló <80465879+hugodiasg@users.noreply.github.com>
2023-08-04 17:57:04 -0300

    Update 12-openlane.sh

--- 12-openlane.sh
+++ 12-openlane.sh
@@ -8,7 +8,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 
 sudo usermod -aG docker $USER
 sudo chmod 666 /var/run/docker.sock
